ba79a085f143994dad71aa06a477ff7006097280
protofluff <protofluff@outlook.com>
2022-12-02 21:36:45 +0100

    Add automatic version picking in config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
-VERSION = '1.0.0'
+import subprocess
+
+VERSION = str(subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']), 'UTF-8').strip() or '1.0.0'
 AUTHOR = 'protofluff'
 REDDIT_AUTHOR = '_protofluff_'
 APP_ID = 'dev.protofluff.gamedeals'
