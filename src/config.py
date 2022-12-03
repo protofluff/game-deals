80f3368c0e216f2170fa4d6fcc59fffd41559714
protofluff <protofluff@outlook.com>
2022-12-03 09:53:43 +0100

    Fix config file

--- src/config.py
+++ src/config.py
@@ -1,6 +1,4 @@
-import subprocess
-
-VERSION = str(subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']), 'UTF-8').strip() or '1.0.0'
+VERSION = '1.1.1'
 AUTHOR = 'protofluff'
 REDDIT_AUTHOR = '_protofluff_'
 APP_ID = 'dev.protofluff.gamedeals'
